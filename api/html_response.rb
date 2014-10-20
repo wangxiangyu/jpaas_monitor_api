281cda5d2ce4c82daea1f0583c78ac70a7f902a8
wangxiangyu <wangxiangyu@baidu.com>
2014-10-20 16:09:16 +0800

    add html page for router 502 log

--- api/html_response.rb
+++ api/html_response.rb
@@ -48,5 +48,128 @@ module Acme
         }
         ERB.new(response_template).result(binding)
     end
+
+    get "/html/router/router_502_log" do
+        header 'Content-Type', 'text/html'
+        #error_apps=JSON.parse(Net::HTTP.get('127.0.0.1','/check/instance_num_blance_check_ng00_ng01',8002))
+        response_template=%{
+        <!DOCTYPE html>
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                <title>router_502_log</title>
+                <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
+            </head>
+            <body>
+                <form name="input" action="router_502_log_sum" method="get">
+                    <input type="checkbox" name="router" />
+                    router
+                    <input type="checkbox" name="cluster" />
+                    cluster
+                    <input type="checkbox" name="app_id" />
+                    app_id
+                    <input type="checkbox" name="error_type" />
+                    error_type
+                    <input type="checkbox" name="instance_addr" />
+                    instance_addr
+                    <input type="checkbox" name="idc" />
+                    idc
+                    <input type="submit" value="Submit" />
+                </form>
+             </body>
+        </html>
+        }
+        ERB.new(response_template).result(binding)
+    end
+
+
+    desc "get 502 info (num sum) from router.log html response"
+    params do
+    #    requires :from_time, type: String, desc: "start time"
+    #    requires :to_time, type: String, desc: "end time"
+        optional :idc, type: String, desc: "show idc or not", default: 'no'
+        optional :instance_addr, type: String, desc: "show instance_addr or not", default: 'no'
+        optional :error_type, type: String, desc: "show error_type or not", default: 'no'
+        optional :app_id, type: String, desc: "show app_id or not", default: 'no'
+        optional :cluster, type: String, desc: "show cluster or not", default: 'no'
+        optional :router, type: String, desc: "show router or not", default: 'no'
+    end
+    get "/html/router/router_502_log_sum" do
+        header 'Content-Type', 'text/html'
+        idc_need=format(params['idc'])
+        instance_addr_need=format(params['instance_addr'])
+        error_type_need=format(params['error_type'])
+        app_id_need=format(params['app_id'])
+        cluster_need=format(params['cluster'])
+        router_need=format(params['router'])
+        request=""
+        request=request+"idc=yes"+"&" if idc_need == 'on'
+        request=request+"instance_addr=yes"+"&" if instance_addr_need == 'on'
+        request=request+"error_type=yes"+"&" if error_type_need == 'on'
+        request=request+"app_id=yes"+"&" if app_id_need == 'on'
+        request=request+"cluster=yes"+"&" if cluster_need == 'on'
+        request=request+"router=yes"+"&" if router_need == 'on'
+        request.sub!(/&$/,'')
+        router_502_sum=JSON.parse(Net::HTTP.get('127.0.0.1',"/routerlog/get_502_sum?from_time=-1000s&to_time=now&#{request}",8002))
+        response_template=%{
+        <!DOCTYPE html>
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                <title>router_502_log</title>
+                <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
+            </head>
+            <body>
+                <table border="1" align="center">
+                    <tr>
+                        <% if router_need == 'on' %>
+                            <td align="center" >router</td>
+                        <% end %>
+                        <% if cluster_need == 'on' %>
+                            <td align="center" >cluster</td>
+                        <% end %>
+                        <% if app_id_need == 'on' %>
+                            <td align="center" >app_id</td>
+                        <% end %>
+                        <% if error_type_need == 'on' %>
+                            <td align="center" >error_type</td>
+                        <% end %>
+                        <% if instance_addr_need == 'on' %>
+                            <td align="center" >instance_addr</td>
+                        <% end %>
+                        <% if idc_need == 'on' %>
+                            <td align="center" >idc</td>
+                        <% end %>
+                            <td align="center" >count</td>
+                    </tr>
+                    <% router_502_sum.each do |info| %>
+                        <tr>
+                            <% if info.has_key?("router") %>
+                                <td align="center" ><%= info['router'] %></td>
+                            <% end %>
+                            <% if info.has_key?("cluster") %>
+                                <td align="center" ><%= info['cluster'] %></td>
+                            <% end %>
+                            <% if info.has_key?("app_id") %>
+                                <td align="center" ><%= info['app_id'] %></td>
+                            <% end %>
+                            <% if info.has_key?("error_type") %>
+                                <td align="center" ><%= info['error_type'] %></td>
+                            <% end %>
+                            <% if info.has_key?("instance_addr") %>
+                                <td align="center" ><%= info['instance_addr'] %></td>
+                            <% end %>
+                            <% if info.has_key?("idc") %>
+                                <td align="center" ><%= info['idc'] %></td>
+                            <% end %>
+                                <td align="center" ><%= info['count'] %></td>
+                        </tr>
+                    <% end %>
+                </table>
+             </body>
+        </html>
+        }
+        ERB.new(response_template).result(binding)
+    end
   end
 end
--- api/router_log.rb
+++ api/router_log.rb
@@ -13,7 +13,7 @@ module Acme
         after do
             ActiveRecord::Base.clear_active_connections!
         end
-        desc "get 502 info from router.log"
+        desc "get 502 info (detail)from router.log"
         params do
             requires :from_time, type: String, desc: "start time"
             requires :to_time, type: String, desc: "end time"
@@ -72,6 +72,82 @@ module Acme
                 result << result_event
             end
             return result
+        end
+        desc "get 502 info (num sum) from router.log"
+        params do
+            requires :from_time, type: String, desc: "start time"
+            requires :to_time, type: String, desc: "end time"
+            optional :idc, type: String, desc: "show idc or not" , values: ['yes','no'], default: 'no'
+            optional :instance_addr, type: String, desc: "show instance_addr or not", values: ['yes','no'], default: 'no'
+            optional :error_type, type: String, desc: "show error_type or not", values: ['yes','no'], default: 'no'
+            optional :app_id, type: String, desc: "show app_id or not", values: ['yes','no'], default: 'no'
+            optional :cluster, type: String, desc: "show cluster or not", values: ['yes','no'], default: 'no'
+            optional :router, type: String, desc: "show router or not", values: ['yes','no'], default: 'no'
+        end
+        get '/get_502_sum' do
+            from_time=format(params['from_time'])
+            to_time=format(params['to_time'])
+            idc_need=format(params['idc'])
+            instance_addr_need=format(params['instance_addr'])
+            error_type_need=format(params['error_type'])
+            app_id_need=format(params['app_id'])
+            cluster_need=format(params['cluster'])
+            router_need=format(params['router'])
+            result={}
+            result_from_jiuze=JSON.parse(Net::HTTP.get("api.aqueducts.baidu.com","/v1/events?product=jpaas&service=router-error&item=page_view&calculation=count&from=#{from_time}&to=#{to_time}&period=10&detail=true",80))
+            return result if result_from_jiuze.size==0
+            result_from_jiuze.each do |event|
+                event["tags"]["idc&instance_addr&error_type&app_id&cluster&router&idc"].each do |info,count|
+                    info_array=info.split('&')
+                    idc=info_array[0]
+                    instance_addr=info_array[1]
+                    error_type=info_array[2]
+                    app_id=info_array[3]
+                    cluster=info_array[4]
+                    router=info_array[5]
+                    key=''
+                    key=key+idc+'&' if idc_need == 'yes'
+                    key=key+instance_addr+'&' if instance_addr_need == 'yes'
+                    key=key+error_type+'&' if error_type_need == 'yes'
+                    key=key+app_id+'&' if app_id_need == 'yes'
+                    key=key+cluster+'&' if cluster_need == 'yes'
+                    key=key+router if router_need == 'yes'
+                    key.sub!(/&$/,'')
+                    if result.has_key?(key)
+                        result[key]['count']+=count.to_i
+                    else
+                        result[key]={}
+                        result[key]['idc']=idc if idc_need == 'yes'
+                        result[key]['instance_addr']=instance_addr if instance_addr_need == 'yes'
+                        result[key]['error_type']=error_type if error_type_need == 'yes'
+                        result[key]['app_id']=app_id if app_id_need == 'yes'
+                        result[key]['cluster']=cluster if cluster_need == 'yes'
+                        result[key]['router']=router if router_need == 'yes'
+                        result[key]['count']=count.to_i
+                    end
+                end
+            end
+            result_sort=[]
+            p result
+            result.each do |key,info|
+                if result_sort.size ==0
+                    result_sort << info
+                else
+                    index=0
+                    result_sort.each do |item|
+                        if info['count'] >= item['count']
+                            result_sort.insert(index,info)
+                            break
+                        end
+                        if index==result_sort.size-1
+                            result_sort.insert(result_sort.size,info)
+                            break
+                        end
+                        index=index+1
+                    end
+                end
+            end
+            return result_sort
         end
     end
   end
